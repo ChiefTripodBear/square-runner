fe1205fa3ed3f0142363e1b232c2701adbb17162
ChiefTripodBear <avalacos@gmail.com>
2019-05-19 20:37:57 +1000

    first commit

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TilePos { raised, neutral, lowered};
+
+public class GroundTile : MonoBehaviour
+{
+    public TilePos tilePos;                                                         //capture if this tile is raised, or lowered
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
